645a4cdc4b47475e239793ab56af0ddac2cd3fb3
Nathan Du <nathandu@outlook.com>
2025-01-15 11:14:56 +0800

    move large methods out of line

--- src/LCDI2C_Katakana.h
+++ src/LCDI2C_Katakana.h
@@ -37,46 +37,8 @@ public:
 
 protected:
   uint8_t nextWordLength(const byte text[], uint16_t start,
-                         uint16_t code) override {
-    // Check for Katakana words with diacritic
-    for (uint8_t idx = 0; idx < KanaDiacriticCharacterNum; idx++)
-      if (KanaDiacriticCharacters[idx].code == code)
-        return 2;
-
-    // Check for other Katakana words
-    if (code >= 0x30A1 && code <= 0x30F3)
-      return 1;
-
-    // Check for English word
-    return LCDI2C_UTF8::nextWordLength(text, start, code);
-  };
-
-  uint16_t getCharacter(uint16_t code) override {
-    uint8_t base, diacritic = 0;
-
-    // Look for katakana
-    if (code >= 0x30A0 && code < 0x3100) {
-      // Separate one Katakana character with diacritic into base character and
-      // diacritic character
-      for (uint8_t idx = 0; idx < KanaDiacriticCharacterNum; idx++) {
-        if (KanaDiacriticCharacters[idx].code == code) {
-          code = KanaDiacriticCharacters[idx].base;
-          diacritic = KanaDiacriticCharacters[idx].diacritic;
-        }
-      }
-    }
-
-    // Look for Latin Fullwidth character
-    if (code >= 0xFF01 && code <= 0xFF5D)
-      return code - 0xFEE0; // Convert Latin Fullwidth to Latin ASCII
-
-    base = LCDI2C_UTF8::getCharacter(code);
-
-    if (diacritic == 0)
-      return (uint16_t)base;
-    else
-      return uint16_t(base) | (diacritic << 8);
-  }
+                         uint16_t code) override;
+  uint16_t getCharacter(uint16_t code) override;
 };
 
 typedef LCDI2C_Katakana LiquidCrystal_I2C_Katakana;
--- src/LCDI2C_RussianLatin.h
+++ src/LCDI2C_RussianLatin.h
@@ -32,38 +32,7 @@ public:
   }
 
 protected:
-  uint16_t getCharacter(uint16_t code) override {
-    uint8_t ord;
-
-    if (code < 0x7E || // ASCII character?
-        (code <= maxExcludedChar &&
-         code >= minExcludedChar)) { // Exclude a selected range in a LCD's ROM
-                                     // character set
-      if (code > 0x1F)
-        return (uint16_t)code; // Printable
-      else
-        return (uint16_t)'*'; // Non-printable
-    }
-
-    // Look for Unicode character in CGRAM (search in CGRAM first)
-    if (customizedLanguage != NULL) {
-      ord = customizedLanguage->getCharacter(code, cursorColumn, cursorRow);
-      if (ord != NOTFOUND)    // Character is in CGRAM?
-        return (uint16_t)ord; // Return customized character
-    }
-
-    // Look for Unicode Latin letter in ROM
-    if (code >= 0xA1 &&
-        code <= 0xFF) // Latin characters have the same code in ROM
-      return code;    // so simply return it
-
-    // Look for Unicode Russian letter in ROM
-    ord = getROMCharacter(code);
-    if (ord != NOTFOUND)    // Character is in ROM?
-      return (uint16_t)ord; // Return ROM character
-
-    return (uint16_t)'*'; // Unknown character
-  }
+  uint16_t getCharacter(uint16_t code) override;
 };
 
 typedef LCDI2C_RussianLatin LiquidCrystal_I2C_RussianLatin;
