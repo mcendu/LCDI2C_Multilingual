b242880607c7b7a84868c6bf0502fd4a363017f2
Nathan Du <nathandu@outlook.com>
2025-01-15 11:31:31 +0800

    replace non-standard binary number literal with hex

--- src/base/LCDI2C.h
+++ src/base/LCDI2C.h
@@ -51,9 +51,9 @@
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
-#define En B00000100 // Enable bit
-#define Rw B00000010 // Read/Write bit
-#define Rs B00000001 // Register select bit
+#define En 0x04 // Enable bit
+#define Rw 0x02 // Read/Write bit
+#define Rs 0x01 // Register select bit
 
 class LCDI2C : public Print {
 public:
